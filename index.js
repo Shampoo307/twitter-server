efe73901a6e95435cc77cfca56faa449253db909
Hickro <79234709+Hickro@users.noreply.github.com>
2021-10-19 15:20:08 +1000

    Added in a sentiment analysis API. Currently hardcoded text, need to update with tweet text

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get('/search/:searchTerm', (req, res) => {
     const searchterm = req.params.searchTerm;
     const query = `%23${searchterm}`;
     const getAuth = getAccessToken();
+    const getAnalysis = getSentiment();
     console.log("QUERY = " + query);
 
     getAuth.then((token) => {
@@ -31,7 +32,7 @@ app.get('/search/:searchTerm', (req, res) => {
             .then((response) => {
                 const tweets = response.data;
                 res.send(tweets);
-                console.log('Response: ', response.data);
+                //console.log('Response: ', response.data);
             })
             .catch((err) => {
                 if (err.repsonse) {
@@ -44,10 +45,36 @@ app.get('/search/:searchTerm', (req, res) => {
                 }
             });
     });
+
+    //Sentiment call
+    getAnalysis.then((token) => {
+
+        var options = {
+            method: 'GET',
+            url: 'https://twinword-sentiment-analysis.p.rapidapi.com/analyze/',
+            params: {text: "This is a bad hardcoded string that is being analysed"},
+            headers: {
+              'x-rapidapi-host': 'twinword-sentiment-analysis.p.rapidapi.com',
+              'x-rapidapi-key': '6a6207d250mshd240de65ef5af7fp1cc03bjsnd422221fa355'
+            }
+          };
+          
+          axios.request(options).then(function (response) {
+            console.log('ANALYSIS Response: ', response.data);
+          }).catch(function (error) {
+                if (error.repsonse) {
+                        console.log('Error in Analysis Search Response');
+                    } else if (error.request) {
+                        console.log('Error in Analysis Search Request');
+                        //console.log(error.request);
+                    } else {
+                        console.log('Error retrieving Analysis search result');
+                    }
+        });
+    }); 
 });
 
 async function getAccessToken() {
-
     const client_id = process.env.API_KEY;
     const client_secret = process.env.API_KEY_SECRET;
 
@@ -67,9 +94,38 @@ async function getAccessToken() {
             } else if (err.request) {
                 console.log('Error in Auth Request');
             } else {
-                console.log('Error retrieving Access Token');
+                console.log('Error retrieving Analysis Access Token');
             }
         });
+}
+
+//Pls don't spam as after 500 calls I will get billed 0.003 per call lmao. Going to upgrade just waiting to confirm
+async function getSentiment() {
+    //const API_KEY = process.env.SENTIMENT_KEY;
+    var options = {
+        method: 'POST',
+        url: 'https://twinword-sentiment-analysis.p.rapidapi.com/analyze/',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'x-rapidapi-host': 'twinword-sentiment-analysis.p.rapidapi.com',
+            'x-rapidapi-key': '6a6207d250mshd240de65ef5af7fp1cc03bjsnd422221fa355'
+        },
+        data: { text: 'great value in its price range!' }
+    };
+
+    return await axios.request(options).then(function (response) {
+        console.log("SENTIMENT WORKS-----------")
+        console.log(response.data);
+    }).catch(function (error) {
+        if (error.repsonse) {
+            console.log('Error in Analysis Auth Response');
+        } else if (error.request) {
+            console.log('Error in Analysis Auth Request');
+        } else {
+            console.log('Error retrieving Access Token');
+        }
+
+    });
 }
 
 app.use((req, res) => {
